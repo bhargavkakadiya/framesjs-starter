33bc056d7251393d2fb331a7f47e259183d6e139
shyam-patel-kira <shyampkira@gmail.com>
2024-03-24 11:16:37 -0400

    Add pinata fdk for analytics

---
+++ server/pinata.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+import { PinataFDK } from "pinata-fdk";
+export const fdk = new PinataFDK({
+	pinata_jwt: process.env.PINATA_JWT as string,
+	pinata_gateway: process.env.PINATA_GATEWAY as string,
+});
